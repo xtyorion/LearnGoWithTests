cdbcdfad55b19ccaa81f15133f33a54605897897
mejoradavincentflorence <mejoradavincentflorence@gmail.com>
2023-01-30 11:04:06 -0700

    updated and tested Hello()

--- hello/hello.go
+++ hello/hello.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 )
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 func main() {
 	fmt.Println(Hello("world"))
