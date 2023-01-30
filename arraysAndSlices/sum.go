0d9d97340aa2843dc51c1faf951c1a4741a36225
mejoradavincentflorence <mejoradavincentflorence@gmail.com>
2023-01-30 14:13:47 -0700

    [Arrays and Slices] built and tested this feature

---
+++ arraysAndSlices/sum.go
@@ -0,0 +1,34 @@
+package arraysAndSlices
+
+func Sum(numbers []int) (sum int) {
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
